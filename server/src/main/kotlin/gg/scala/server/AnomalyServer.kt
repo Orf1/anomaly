43e70e2074b7915b1fd4330c3527b5d519a89ec5
Orf1 <orfeas126@gmail.com>
2021-12-23 23:55:17 -0800

    Removed Python Stuff

--- server/src/main/kotlin/gg/scala/server/AnomalyServer.kt
+++ server/src/main/kotlin/gg/scala/server/AnomalyServer.kt
@@ -1,41 +1,5 @@
 package gg.scala.server
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.ObjectOutputStream
-import java.net.ServerSocket
-
-
 class AnomalyServer {
-    private val port = 5000
-    private val pythonServerSocket: ServerSocket = ServerSocket(port)
 
-    init {
-        connectPython()
-    }
-
-    private fun connectPython() {
-        println("Starting python server thread.")
-        println("Python server thread listening for connections.")
-        while (true) {
-            val socket = pythonServerSocket.accept()
-            println("Client connected!")
-            val inFromClient = BufferedReader(InputStreamReader(socket.getInputStream()))
-            val outToClient = socket.getOutputStream().bufferedWriter()
-            outToClient.write("Test")
-            while (true) {
-                val incoming = inFromClient.readLine()
-                println("Received: $incoming")
-                if (incoming.equals("q")) {
-                    socket.close()
-                    break
-                }
-            }
-        }
-
-    }
-}
-
-fun main() {
-    AnomalyServer()
 }
