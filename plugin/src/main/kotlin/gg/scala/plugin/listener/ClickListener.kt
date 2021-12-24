e23e364802739204b6fb2b5dc5b99113930c1f52
Orf1 <orfeas126@gmail.com>
2021-12-23 23:56:45 -0800

    Reformatted some stuff

--- plugin/src/main/kotlin/gg/scala/plugin/listener/ClickListener.kt
+++ plugin/src/main/kotlin/gg/scala/plugin/listener/ClickListener.kt
@@ -7,41 +7,35 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 import java.util.*
 
-class ClickListener(val plugin: AnomalyPlugin) : Listener
-{
+class ClickListener(val plugin: AnomalyPlugin) : Listener {
 
     private val startMeasuring = hashMapOf<UUID, Long>()
     private val clickMap = hashMapOf<UUID, MutableList<Long>>()
 
     @EventHandler
-    fun click(event: PlayerInteractEvent)
-    {
+    fun click(event: PlayerInteractEvent) {
         val player = event.player
 
-        if (startMeasuring.containsKey(player.uniqueId))
-        {
+        if (startMeasuring.containsKey(player.uniqueId)) {
             val startTime = startMeasuring[player.uniqueId]!!
 
-            if (System.currentTimeMillis() - startTime <= 5000)
-            {
-                if (event.action.name.contains("LEFT"))
-                {
+            if (System.currentTimeMillis() - startTime <= 5000) {
+                if (event.action.name.contains("LEFT")) {
                     clickMap
                         .putIfAbsent(player.uniqueId, mutableListOf())!!
                         .add(System.currentTimeMillis() - startTime)
                 }
-            } else
-            {
+            } else {
 
 
                 clickMap[player.uniqueId]?.get(0)
                 player.sendMessage("Total Clicks: ${clickMap[player.uniqueId]!!.size}")
-                clickMap[player.uniqueId]?.forEach { player.sendMessage("" +  it); }
-                for (i in 0..clickMap.size-1) {
+                clickMap[player.uniqueId]?.forEach { player.sendMessage("" + it); }
+                for (i in 0..clickMap.size - 1) {
                     if (i == 0) {
                         player.sendMessage("Click Offsets:")
                     } else {
-                        val offset = clickMap[player.uniqueId]!![i] - clickMap[player.uniqueId]!![i-1]
+                        val offset = clickMap[player.uniqueId]!![i] - clickMap[player.uniqueId]!![i - 1]
                         player.sendMessage("Offset: $offset")
                     }
                 }
